30e1c70fddc51a32cbc68f93730d5f9112cae8ef
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-03-19 18:40:57 +0900

    Feat : 네비바 로그인 유무 상태 기능 추가

--- frontend/src/MyRoute.js
+++ frontend/src/MyRoute.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import Main from './component/Main';
-import Auth from './component/Auth';
-import Form from './component/Form';
-import { Fragment, useState } from 'react';
-function MyRoute(){
-    const [isLoggedIn, setIsLoggedIn]=useState(false);
-    return(
-    <Fragment> 
-    <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/auth' element={<Auth/>}/>
-        <Route path='/form' element={<Form/>}/>
-    </Routes>
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./component/Main";
+import Auth from "./component/Auth";
+import Form from "./component/Form";
+import { Fragment, useState } from "react";
+function MyRoute() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<Main isLoggedIn={isLoggedIn} />} />
+        <Route path="/auth" element={<Auth setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
     </Fragment>
-    );
+  );
 }
 export default MyRoute;
