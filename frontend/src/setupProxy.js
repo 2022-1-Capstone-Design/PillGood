1af1cf97ec9751b91ce68b9dacae986eefd94f62
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-03-21 14:10:26 +0900

    Feat: add setupProxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/", {
+    createProxyMiddleware("/api", {
+      target: "http://localhost:5000",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    createProxyMiddleware("/user", {
       target: "http://localhost:5000",
       changeOrigin: true,
     })
