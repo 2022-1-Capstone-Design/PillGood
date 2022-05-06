aa376bcbf7875d41b72dd7651e021993bfd49c18
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-05-06 16:28:52 +0900

    feat: 데이터셋 변경

--- backend/example.py
+++ backend/example.py
@@ -2,14 +2,30 @@ import sys
 import base64
 import io
 import json
+import numpy as np
+import pandas as pd
+'''
+pill_train = pd.read_csv('MinMax_Pill_data.csv', header=0, index_col=0, encoding='cp949')
+
+def distance(x, y):
+    x= x.to_numpy()
+    a = np.linalg.norm(x-y)
+    return a
+
+A1 = [0, 0, 2.25, 1.43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+distance_list = []
+
+for i in range(len(pill_train)):
+    v = distance(pill_train.iloc[i], A1)
+    distance_list.append(v)
+'''
 
 if __name__ == "__main__":
     
     sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
     sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
     vString = sys.argv[1]
-    result = {
-        "혈관/혈액순환": [[1, 2, 3], [4,5,6]]
-    }
     vJson = json.loads(vString)
-    print("dict = ", result)
+    print("dict = ", vJson)
+    
