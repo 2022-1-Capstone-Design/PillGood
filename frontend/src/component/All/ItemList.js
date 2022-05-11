cf8c79ff64d49b5079fb1bb546a56a889394c2d5
minjiLeee <96130585+minjiLeee@users.noreply.github.com>
2022-05-11 20:29:37 +0900

    Feat. 전체보기 페이지네이션 추가

--- frontend/src/component/All/ItemList.js
+++ frontend/src/component/All/ItemList.js
@@ -1,73 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import ShowItem from './ShowItem';
-import axios from 'axios';
-import '../../css/All/ItemList.css';
+import React, { useState, useEffect } from "react";
+import ShowItem from "./ShowItem";
+import axios from "axios";
+import "../../css/All/ItemList.css";
+import Pagination from "./Pagination";
 
 const ItemList = () => {
-    const [products, setProducts] = useState(null);
-    const [search, setSearch]=useState("");
-    const [btnClick, setBtnClick]=useState(false);
-    const [product, setProduct]=useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(20);
+  const [search, setSearch] = useState("");
+  const [btnClick, setBtnClick] = useState(false);
+  const [product, setProduct] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-       
-            try {
-                const response = await axios.get('/product');
-               setProducts(response.data);
-            } catch (e) {
-                console.log(e);
-            }
-      
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios.get("/product");
+      setProducts(response.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
-    if (!products) {
-        return null;
-    }
-    
-    const writePill=(e)=>{
-        setSearch(e.target.value);
-      }
-    
-    const searchPill=(e)=>{
-        e.preventDefault();
-        console.log(search);
-        setBtnClick(true);
-        setProduct(
-            products.filter(item=>item.PRDLST_NM.includes(e.target.pname.value)
-            || item.BSSH_NM.includes(e.target.pname.value))
-        )
-        console.log(product);
-    }
+  if (!products) {
+    return null;
+  }
 
-    return (
-        <div>
-        <form onSubmit={searchPill}>
-            <input type="text" name="pname" placeholder="영양제 검색하기"
-            value={search} onChange={writePill}/>
-            <input type="submit" name="btn" value='검색하기'/>
-        </form>
-    
-        <div className="list-block">
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
 
-            {btnClick?
-                product.map(item=>{
-                  return <ShowItem key={item.INDEX} items={item}/>
-                }
-                )
-                
-            : 
-            products.map(items => {
-                return <ShowItem key={items.INDEX} items={items} />
-            })
-            }
-            
-        </div>
-        </div>
-            
+  const writePill = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchPill = (e) => {
+    e.preventDefault();
+    console.log(search);
+    setBtnClick(true);
+    setProduct(
+      products.filter(
+        (item) =>
+          item.PRDLST_NM.includes(e.target.pname.value) ||
+          item.BSSH_NM.includes(e.target.pname.value)
+      )
     );
+    console.log(product);
+  };
+
+  return (
+    <div>
+    <form onSubmit={searchPill}>
+        <input
+          type="text"
+          name="pname"
+          placeholder="영양제 검색하기"
+          value={search}
+          onChange={writePill}
+        />
+        <input type="submit" name="btn" value="검색하기" />
+      </form>
+
+    <div className="list-block">
+      <ShowItem products={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={products.length}
+        paginate={setCurrentPage}
+      ></Pagination>
+    </div>
+    </div>
+  );
 };
 
 export default ItemList;
--- frontend/src/component/All/ShowItem.js
+++ frontend/src/component/All/ShowItem.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import '../../css/All/ShowItem.css';
+import React from "react";
+import "../../css/All/ShowItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faHeart} from "@fortawesome/free-regular-svg-icons";
 
-const ShowItem = ({ items }) => {
-    const { INDEX, PRDLST_NM, BSSH_NM } = items;
-    const path=`..\\..\\..\\img\\${INDEX}.jpg`;
-
-    return (
-        <div className="item-card">
-        <h2>{INDEX}</h2>
-        <img src={path} alt="" />
-        <p>{PRDLST_NM}</p>
-        <p>{BSSH_NM}</p>
-        <FontAwesomeIcon icon={faHeart} />
-        </div>
-    );
+const ShowItem = ({ products }) => {
+  return (
+    <ul className="list">
+      {products.map((product) => (
+        <li key={product.INDEX} className="item-card">
+          <p>{product.PRDLST_NM}</p>
+          <p>{product.BSSH_NM}</p>
+          <img src={`..\\..\\..\\img\\${product.INDEX}.jpg`} alt="" />
+          <FontAwesomeIcon icon={faHeart} />
+        </li>
+      ))}
+    </ul>
+  );
 };
-
 export default ShowItem;
