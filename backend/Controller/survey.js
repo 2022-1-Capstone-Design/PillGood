e3e822060d9f1e7bafabc863e0f362789033960d
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-05-13 16:56:45 +0900

    fix: 머지 후 오류 해결

--- backend/Controller/survey.js
+++ backend/Controller/survey.js
@@ -12,64 +12,6 @@ const getQuestion = async (req, res) => {
 };
 
 const getResult = async (req, res) => {
-  try {
-    console.log(req.body);
-    let result = new Object();
-    for (elements of req.body.surveyAnswer) {
-      if (typeof elements === "object") {
-        for (element of elements) {
-          const question = await Question.aggregate([
-            {
-              $unwind: {
-                path: "$sub_category",
-              },
-            },
-            {
-              $unwind: {
-                path: "$sub_category.question",
-              },
-            },
-            {
-              $match: {
-                "sub_category.question._id": new ObjectId(element),
-              },
-            },
-          ]);
-          const score = await Score.aggregate([
-            {
-              $match: {
-                ref: new ObjectId(element),
-              },
-            },
-          ]);
-          if (result[question[0].main_category] === undefined)
-            result[question[0].main_category] = new Object();
-          result[question[0].main_category][question[0].sub_category.name] ===
-          undefined
-            ? (result[question[0].main_category][
-                question[0].sub_category.name
-              ] = [])
-            : result[question[0].main_category][
-                question[0].sub_category.name
-              ].push(score[0].score);
-        }
-      }
-    }
-    const spawn = require("child_process").spawn;
-    const python = spawn("python", ["example.py", JSON.stringify(result)]);
-    python.stdout.on("data", (data) => {
-      let buff = Buffer.from(data, "base64");
-      let text = buff.toString("utf-8");
-      const list = eval(text);
-      console.log(list);
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json(error);
-  }
-};
-
-const getResult = async (req, res) => {
     try {
         const surveyAnswer = req.body.surveyAnswer;
         let result = {
@@ -118,6 +60,9 @@ const getResult = async (req, res) => {
             let text = buff.toString("utf-8");
             console.log(text);
         });
+        python.stderr.on("data", (data)=> {
+          console.error(data.toString( ));
+        })
     } catch (error) {
         console.error(error);
         return res.status(500).json(error);
--- backend/Router/survey.js
+++ backend/Router/survey.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const Survey = require("../Controller/survey");
 
 router.get("/", Survey.getQuestion); 
-<<<<<<< Updated upstream
-router.post("/", Survey. getResult);
-=======
+
 router.post("/", Survey.getResult);
->>>>>>> Stashed changes
+
 
 module.exports = router;
