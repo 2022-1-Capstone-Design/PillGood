88cf0d5bd190c2e913f739bb3580b6a63f78ee15
heenahan <mby1997s@gmail.com>
2022-05-13 16:16:39 +0900

    Feat: 전체보기 검색기능

--- backend/Controller/product.js
+++ backend/Controller/product.js
@@ -1,11 +1,24 @@
 const Product = require('../Schemas/product')
+const url = require("url");
 
 const getProduct = async (req, res) => {
     try {
-       const products = await Product.find( ); 
-       return res.status(200).json(products);
+        const params = url.parse(req.url, true).query;
+        let products
+        if (params.search) {
+            const split = params.search.split(' ');
+            const arr = split.map(e => new RegExp(e))
+            products = await Product.find( 
+                {
+                    PRDLST_NM: arr
+                }
+            );
+        } else {
+            products = await Product.find( );
+        }
+        return res.status(200).json(products);
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json(error);
     } 
 }
 
