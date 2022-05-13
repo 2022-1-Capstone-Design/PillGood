e6fe5df8ed8260afc7073425bb13a11c003a9682
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-05-13 20:42:33 +0900

    Create result.js

---
+++ backend/Schemas/result.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+
+const pillfoodSchema = new Schema({
+    category_name: [String],
+    product: [Number],
+    food: [Number],
+    nutrient: [String]
+});
+
+const bmiSchema = new Schema({
+    bmi_figure: Number,
+    bmi_result: String
+});
+
+const resultSchema = new Schema({
+    kakao_id: Number,
+    user_date: {type: Date, default: () => new Date()},
+    user_name: String,
+    BMI: [bmiSchema],
+    age: Number,
+    result: [pillfoodSchema]
+}, {versionKey: false});
+
+module.exports = mongoose.model('Result', resultSchema);
+
