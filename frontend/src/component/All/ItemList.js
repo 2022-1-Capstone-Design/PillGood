c9f432c6c4f076c3a223c0c30eca502760f970e9
minjiLeee <96130585+minjiLeee@users.noreply.github.com>
2022-05-14 19:22:36 +0900

    Feat. 전체보기 페이지 번호 분리 완료

--- frontend/src/component/All/ItemList.js
+++ frontend/src/component/All/ItemList.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import {useLocation, useNavigate} from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import ShowItem from "./ShowItem";
 import axios from "axios";
 import "../../css/All/ItemList.css";
-import Pagination from "./Pagination";
 
 const ItemList = () => {
   const [products, setProducts] = useState([]);
@@ -12,26 +11,52 @@ const ItemList = () => {
   const [postsPerPage] = useState(20);
   const [search, setSearch] = useState("");
   const [btnClick, setBtnClick] = useState(false);
-  const [product, setProduct] = useState([]);
   const navigate = useNavigate();
-  const location=useLocation();
-    
+  const location = useLocation();
+  const [pageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+  const handleClick = (event) => {
+    setCurrentPage(Number(event.target.id));
+  };
+
+  const pageNumber = [];
+  for (let i = 1; i <= Math.ceil(products.length / postsPerPage); i++) {
+    pageNumber.push(i);
+  }
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
+
+  const renderPageNumbers = pageNumber.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={handleClick}
+          className={currentPage === number ? "active" : null}
+        >
+          {number}
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+
   useEffect(() => {
     const fetchData = async () => {
-        const nutrients=[
-            "루테인", "비타민A", "비타민D", "비타민E","비타민K", "비타민C", "비타민B2",
-            "아연","셀렌","철","마그네슘","EPA","프로바이오틱스","실리마린","나이아신",
-            "올리고당","칼슘","비타민B6","베타카로틴","판토텐산","비오틴","망간","크롬",
-            "엽산","구리","몰리브덴"
-        ]
-        try{
-            const response = await axios.get("/product" + location.search);
-            setProducts(response.data);
-            setLoading(false);
-        }catch (e) {
-            console.log(e);
-        }
-
+      try {
+        setLoading(true);
+        const response = await axios.get("/product" + location.search);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
     };
     fetchData();
   }, []);
@@ -40,59 +65,80 @@ const ItemList = () => {
     return null;
   }
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
-
   const writePill = (e) => {
     setSearch(e.target.value);
-
   };
 
-   let data;
+  let data;
 
-   const params=new URLSearchParams(location.search);
+  const params = new URLSearchParams(location.search);
 
   const searchPill = (e) => {
     console.log("2. searchPill함수: ");
     console.log(search);
     e.preventDefault();
-    if(search ){    
+    if (search) {
       console.log("3. searchPill함수 내 if문 들어감 :");
       console.log(search);
-    data={key : {search}};
-    //검색단어 저장
-    setBtnClick(true);
-    const axiosData= async()=>{
-            try{
-              console.log("5. 백엔드 요청 들어가기 전 값 확인");
-              console.log(search);
-                const responseData =await axios.get('/product?search=' + search);
+      data = { key: { search } };
+      //검색단어 저장
+      setBtnClick(true);
+      const axiosData = async () => {
+        try {
+          console.log("5. 백엔드 요청 들어가기 전 값 확인");
+          console.log(search);
+          const responseData = await axios.get("/product?search=" + search);
 
-                console.log(responseData.data);
-                setProducts(responseData.data);
-            }catch(e){
-                console.log(e);
-            }
+          console.log(responseData.data);
+          setProducts(responseData.data);
+        } catch (e) {
+          console.log(e);
         }
-        axiosData();
-      }
-    };
+      };
+      axiosData();
+    }
+  };
+//페이지네이션 버튼
+  const handleNextbtn = () => {
+    setCurrentPage(currentPage + 1);
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+  const handlePrevbtn = () => {
+    setCurrentPage(currentPage - 1);
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  let pageIncrementBtn = null;
+  if (pageNumber.length > maxPageNumberLimit) {
+    pageIncrementBtn = <li onClick={handleNextbtn}>&hellip;</li>;
+  }
+
+  let pageDecrementBtn = null;
+  if (minPageNumberLimit >= 1) {
+    pageDecrementBtn = <li onClick={handlePrevbtn}>&hellip;</li>;
+  }
+
   return (
     <div>
-    <form onSubmit={(e)=>{
-      navigate({
-        pathname:"/all",
-        search:`?search=${search}`,
-        data:data,
-        });
-        console.log("4. search값 확인");
-       console.log(location.search);
-       console.log(params.get("search"));
-       searchPill(e);
-    }
-  }
-    >
+      <form
+        onSubmit={(e) => {
+          navigate({
+            pathname: "/all",
+            search: `?search=${search}`,
+            data: data,
+          });
+          console.log("4. search값 확인");
+          console.log(location.search);
+          console.log(params.get("search"));
+          searchPill(e);
+        }}
+      >
         <input
           type="text"
           name="pname"
@@ -101,25 +147,46 @@ const ItemList = () => {
           onChange={writePill}
         />
         <input type="submit" name="btn" value="검색하기" />
-    </form>
+      </form>
 
-    <div className="list-block">
-      {btnClick
-        ?
-          (loading?<div>Loading...</div>:
+      <div className="list-block">
+        {btnClick ? (
+          loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div>
+              <ShowItem products={currentPosts} loading={loading} />
+            </div>
+          )
+        ) : loading ? (
+          <div>Loading...</div>
+        ) : (
           <div>
-              <ShowItem products={currentPosts} loading={loading}/>
-          </div>)
-        :
-          (loading?<div>Loading...</div>:
-          <div>
-              <ShowItem products={currentPosts} loading={loading}/>
+            <ShowItem products={currentPosts} loading={loading} />
           </div>
-          )}
-        
-        <Pagination postsPerPage={postsPerPage} totalPosts={products.length} paginate={setCurrentPage}></Pagination>
- 
-    </div>
+        )}
+      </div>
+      <ul className="pageNumbers">
+        <li>
+          <button
+            onClick={handlePrevbtn}
+            disabled={currentPage === pageNumber[0] ? true : false}
+          >
+          &lt;&lt;
+          </button>
+        </li>
+        {pageDecrementBtn}
+        {renderPageNumbers}
+        {pageIncrementBtn}
+        <li>
+          <button
+            onClick={handleNextbtn}
+            disabled={currentPage === pageNumber[pageNumber.length - 1] ? true : false}
+          >
+          &gt;&gt;
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
