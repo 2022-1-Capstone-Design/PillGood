705cf4d730c1e635bc90834779f28f2d46056368
heenahan <mby1997s@gmail.com>
2022-05-15 19:26:34 +0900

    Feat: 백엔드 좋아요 생성, 삭제 완성

--- backend/Controller/product.js
+++ backend/Controller/product.js
@@ -1,10 +1,11 @@
-const Product = require('../Schemas/product')
-const url = require("url");
+const Product = require('../Schemas/product');
+const Like = require("../Schemas/like");
 
 const getProduct = async (req, res) => {
     try {
-        const params = url.parse(req.url, true).query;
-        let products
+        const params = req.query;
+        const user = req.user;
+        let products;
         if (params.search) {
             const split = params.search.split(' ');
             const arr = split.map(e => new RegExp(e))
@@ -18,10 +19,46 @@ const getProduct = async (req, res) => {
         }
         return res.status(200).json(products);
     } catch (error) {
+        console.error(error);
         return res.status(500).json(error);
     } 
+};
+
+const addLike = async (req, res) => {
+    try {
+        Like.findOrCreate({
+            user_id: req.user,
+            product_id: req.body.productId
+        })
+        return res.status(200).json({
+            success: true
+        })
+    } catch(error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+}
+
+const deleteLike = async (req, res) => {
+    try {
+        console.log(req);
+        await Like.deleteOne(
+            {
+                user_id: req.user,
+                product_id: req.body.productId
+            }
+        );
+        return res.status(200).json({
+            success: true
+        })
+    } catch(error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
 }
 
 module.exports = {
     getProduct,
+    addLike,
+    deleteLike
 }
