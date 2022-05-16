67f87084c393f14408c7a9cd570cd3147f985211
heenahan <mby1997s@gmail.com>
2022-05-16 21:16:19 +0900

    Fix: token null이면 JWT.verify 처리하지 않도록 함

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -65,7 +65,7 @@ const logout = async (req, res) => {
 const verifyUser = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        if (JSON.parse(token)) {
+        if (token !== "null") {
             const decryption = jwt.verify(token, process.env.JWT_KEY);
             req.user = decryption.id;
         }
