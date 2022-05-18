3938600182fa65c90848642247a31952d52422b5
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-05-18 18:54:49 +0900

    Feat : 설문조사 결과 페이지 구조 생성

--- frontend/src/component/Survey/Result/SurveyResult.js
+++ frontend/src/component/Survey/Result/SurveyResult.js
@@ -5,14 +5,19 @@ import SurveyResultForm from "./SurveyResultForm";
 
 const SurveyResult = () => {
   const { id } = useParams();
-  const [resultData, setResultData] = useState();
-  useEffect(() => {
-    axios.get(`/survey/${id}`).then((res) => {
-      setResultData(res.data);
-    });
-  }, [id]);
+  const [resultData, setResultData] = useState([]);
 
-  return <div>{<SurveyResultForm resultData={resultData} />}</div>;
+  const getResult = async () => {
+    await axios.get(`/survey/${id}`).then((res) => setResultData(res.data));
+  };
+
+  useEffect(() => {
+    getResult();
+  }, []);
+
+  return (
+    <div>{resultData[0] && <SurveyResultForm resultData={resultData} />}</div> //resultData 검사 안하면 오류남
+  );
 };
 
 export default SurveyResult;
--- frontend/src/component/Survey/Result/SurveyResultForm.js
+++ frontend/src/component/Survey/Result/SurveyResultForm.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SurveyResultForm = ({ resultData }) => {
-  return <div>{resultData[0].user_name}</div>;
+  return (
+    <div>
+      <div>{resultData[0]._id}</div>
+      <br />
+      <h1>{resultData[0].user_name}</h1>
+    </div>
+  );
 };
 
 export default SurveyResultForm;
