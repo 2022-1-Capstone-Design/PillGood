c5a239f314dd3d633ddeee5a26818f6bbff512af
minjiLeee <96130585+minjiLeee@users.noreply.github.com>
2022-05-27 15:02:35 +0900

    Feat. 마이페이지 화면에 뿌리기

--- frontend/src/component/MyPage/ShowMyPage.js
+++ frontend/src/component/MyPage/ShowMyPage.js
@@ -5,8 +5,15 @@ const ShowMyPage = ({ myPageData}) => {
   return (
     <ul className="mypage_list">
         <li key={myPageData.name}>
-         
-        {myPageData.name}
+          {myPageData.name}
+        {myPageData.likes.map(item=>(
+          <div>
+            {item.BSSH_NM}<br/>
+            {item.PRDLST_NM}
+            <img src={`..\\..\\..\\img\\${item.INDEX}.jpg`} alt=''/>
+          </div>
+        ))}
+        <button>지난 결과 보기</button>
         </li>
     </ul>
   );
