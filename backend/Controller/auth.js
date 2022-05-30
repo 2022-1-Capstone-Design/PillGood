c2ce40bbeca038efacd3cc7b647e59bedab67310
heenahan <mby1997s@gmail.com>
2022-05-30 16:59:00 +0900

    Feat: 로그인 여부 확인 쿠키 생성

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -33,8 +33,15 @@ const login = async (req, res) => {
             httpOnly: true,
             sameSite: 'none', // client가 server와 서로 다른 ip라도 동작하도록 함
             secure: true, // sameSite를 none으로 설정했을 경우 secure : true
+            overwrite: true, // 이전에 설정한 동일한 이름의 쿠키 덮어 씌움
         };
-        res.cookie('token', token, options); // cookie에 JWT 담기
+        
+        res.cookie('token', token, options) // cookie에 JWT 담기
+            .cookie('check', true, {
+                maxAge: req.body.expires_in * 1000,
+                sameSite: "none",
+                secure: true,
+            })
 
         return res.status(created? 201 : 200).json({ success: true });
     } catch (error) {
