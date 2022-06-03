1fa8a9508503d8bc9643ca31fb5a20ee2d9600a0
songhee1 <96781855+songhee1@users.noreply.github.com>
2022-06-03 16:20:45 +0900

    Feat : 메인페이지 css 수정
    반응형 수정필요
    (카드 아이템 크기 조절, 휴대폰 접속시 스크롤 양 및 메인페이지 내 각 요소 크기 조절) 필요

--- frontend/src/component/Main.js
+++ frontend/src/component/Main.js
@@ -10,7 +10,7 @@ import Footer from "./Footer";
 import React, { useEffect } from "react";
 import FirstPage from "./Main/FirstPage";
 import SecondPage from './Main/SecondPage';
-
+import ThirdPage from "./Main/ThirdPage";
 function Main({ isLoggedIn, setIsLoggedIn }) {
   const token = window.localStorage.getItem("token");
 
@@ -24,11 +24,11 @@ function Main({ isLoggedIn, setIsLoggedIn }) {
 
 <ReactFullpage
   licenseKey={'YOUR_KEY_HERE'}
-
+  
   render={({state, fullpageApi}) =>{
 
   return (
-    <div>
+    <div className="main-page">
       <ReactFullpage.Wrapper controls controlsProps={{ className: "slide-navigation" }}>
         <div className="section-first">
           <FirstPage/>
@@ -39,41 +39,7 @@ function Main({ isLoggedIn, setIsLoggedIn }) {
         </div>
 
         <div className="section-third">
-          <div className="thirdpage">
-            <h1>당신을 위한 PillGood의 맞춤 영양 케어</h1>
-            <Fade bottom>
-              <div className="thirdpage__">
-                <div className="thirdpage__first">
-                  <div className="circle1"></div>
-                  <div className="text1">
-                    <h2>빠른 설문진단</h2>
-                    <p>
-                      겪고있는 증상을 체크해 영양소를 선별합니다.
-                      <br />
-                      신체발달에 도움을 주는 영양소를 선정합니다.
-                      <br />
-                      10가지 문항을 종합/평가해 웰빙 서비스를 제안합니다.
-                      <br />
-                    </p>
-                  </div>
-                </div>
-
-                <div className="thirdpage__second ">
-                  <div className="circle2"></div>
-                  <div className="text2">
-                    <h2>영양 헬스케어</h2>
-                    <p>
-                      여러 제약회사의 영양제를 알맞게 추천합니다.
-                      <br />
-                      영양소뿐만 아니라, 올바른 식습관을 관리합니다. <br />
-                      치우치지 않은 균형잡힌 건강관리를 돕습니다.
-                      <br />
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </Fade>
-          </div>
+          <ThirdPage/>
         </div>
 
         <div className="section-cards">
