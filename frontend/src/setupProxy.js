dd9841d4dd141abc83418dd94ddc367ebcb926f2
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-06-04 19:37:35 +0900

    Fix : 프록시 서버 주소 배포 서버 주소로 변경

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/api", "/test", "/survey", "/product", "/myPage"], {
-      target: "http://http://ec2-54-224-198-233.compute-1.amazonaws.com",
+      target: "http://ec2-54-224-198-233.compute-1.amazonaws.com",
       changeOrigin: true,
     })
   );
