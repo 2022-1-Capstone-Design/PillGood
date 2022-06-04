8844d5bcda3c9f739cce015ba9aa6a4e53c03649
songhee1 <96781855+songhee1@users.noreply.github.com>
2022-06-05 02:27:41 +0900

    Fix : 메인페이지 css 반응형 수정
    카드 아이템 반응형 수정 필요

--- frontend/src/component/Main/SecondPage.js
+++ frontend/src/component/Main/SecondPage.js
@@ -1,11 +1,10 @@
-import Fade from "react-reveal/Fade";
+
 import pictogram from "../../image/pictogram.png";
 import '../../css/Main/SecondPage.css';
 
 const SecondPage=()=>{
     return(
         <div className="secondpage">
-          <Fade bottom>
             <div className="secondpage__left">
               <span id="secondpage_text1">HEALTH SPECIALIST</span><br/>
               <span id="secondpage_text2">건강 종합 솔루션을 제공하는 PillGood</span>
@@ -18,7 +17,6 @@ const SecondPage=()=>{
             <div className="secondpage__right">
               <img className="secondpage__img" src={pictogram} alt="p"></img>
             </div>
-          </Fade>
         </div>
     );
 }
