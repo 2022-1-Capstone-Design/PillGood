c4507c807c44031e3e8ee121ea4e850b20ceacfb
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-06-04 20:10:35 +0900

    Fix : 배포 서버 주소 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/api", "/test", "/survey", "/product", "/myPage"], {
-      target: "http://ec2-54-224-198-233.compute-1.amazonaws.com:5000",
+      target: "54.224.198.233:5000",
       changeOrigin: true,
     })
   );
