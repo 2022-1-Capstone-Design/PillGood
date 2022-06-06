84e8208e8f93aa4c63ff3ac92e0edeb29641882c
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-06-06 17:47:34 +0900

    fix

--- backend/example.py
+++ backend/example.py
@@ -5,8 +5,8 @@ import test
 
 if __name__ == "__main__":
     
-    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
-    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
     vString = sys.argv[1]
     vJson = json.loads(vString)
     newvJson = test.calc(vJson)
