8d193a43cfed3d9c38376d53358a5bae2e373b77
dydekdnlt <83109168+dydekdnlt@users.noreply.github.com>
2022-06-06 16:56:29 +0900

    Update example.py

--- backend/example.py
+++ backend/example.py
@@ -2,12 +2,11 @@ import sys
 import io
 import json
 import test
-import codecs
 
 if __name__ == "__main__":
     
-    sys.stdout = io.TextIOWrapper(codecs.getwriter("utf-8")(sys.stdout.detach()))
-    sys.stderr = io.TextIOWrapper(codecs.getwriter("utf-8")(sys.stderr.detach()))
+    sys.stdout = io.TextIOWrapper(sys.stdout.reconfigure(encoding='utf-8'))
+    sys.stderr = io.TextIOWrapper(sys.stderr.reconfigure(encoding='utf-8'))
     vString = sys.argv[1]
     vJson = json.loads(vString)
     newvJson = test.calc(vJson)
