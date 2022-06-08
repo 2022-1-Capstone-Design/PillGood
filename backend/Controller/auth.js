9ae0f3945cccb1dfeefee6b42c3737c5151c1767
heenahan <mby1997s@gmail.com>
2022-06-09 01:13:50 +0900

    Feat: request의 쿠키 읽은 뒤 인증

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
       // sameSite: "none", // client가 server와 서로 다른 ip라도 동작하도록 함
       // secure: true, // sameSite를 none으로 설정했을 경우 secure : true
       overwrite: true, // 이전에 설정한 동일한 이름의 쿠키 덮어 씌움
-      domain: ".pillgood.ml",
+      // domain: ".pillgood.ml",
     };
 
     res
@@ -49,7 +49,7 @@ const login = async (req, res) => {
         maxAge: req.body.expires_in * 1000,
         // sameSite: "none",
         // secure: true,
-        domain: ".pillgood.ml",
+        // domain: ".pillgood.ml",
       });
 
     return res.status(created ? 201 : 200).json({ success: true });
@@ -85,10 +85,9 @@ const logout = async (req, res) => {
 
 const verifyUser = async (req, res, next) => {
   try {
-    const cookie = req.headers.cookie;
+    const cookie = req.cookies;
     if (cookie) {
-      const token = cookie.split("=")[1];
-      const decryption = jwt.verify(token.split(";")[0], process.env.JWT_KEY);
+      const decryption = jwt.verify(cookie.token, process.env.JWT_KEY);
       req.user = decryption.id;
     }
     next();
