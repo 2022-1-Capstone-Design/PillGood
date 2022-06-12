448cb9129949c501990ef672adf1e4de8a8e1677
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-06-12 17:35:04 +0900

    Test : login test

--- frontend/src/component/Auth.js
+++ frontend/src/component/Auth.js
@@ -2,22 +2,15 @@ import imgLogin from "../image/kakao_login.png";
 import "../css/Auth.css";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../image/logo2.png";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function Auth({ isLoggedIn }) {
   const navigate = useNavigate();
-  const [cookie, setCookie] = useState(isLoggedIn);
-  const cookies = document.cookie === "";
   useEffect(() => {
-    if (cookies) setCookie(false);
-    else setCookie(true);
-  }, [cookies]);
-  useEffect(() => {
-    if (cookie) {
-      window.location.reload();
-      navigate(-1, true);
-    }
-  }, [cookie]);
+    const cookie = document.cookie !== "";
+    if (cookie) navigate(-1, true);
+  });
+  console.log(isLoggedIn);
   return (
     <div className="auth">
       <Link to="/">
