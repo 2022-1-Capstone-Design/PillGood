99980edb034f3b9ad212d3e9fe6fa2080e01165e
leesb971204 <75251270+leesb971204@users.noreply.github.com>
2022-06-12 16:35:34 +0900

    Test : login test

--- frontend/src/component/Auth.js
+++ frontend/src/component/Auth.js
@@ -6,12 +6,13 @@ import { useEffect } from "react";
 
 function Auth({ isLoggedIn }) {
   const navigate = useNavigate();
+  const cookies = document.cookie();
   useEffect(() => {
-    if (isLoggedIn) {
+    if (cookies) {
       //로그인 상태면 해당 페이지 접근 제한
       navigate(-1, true);
     }
-  }, [isLoggedIn]);
+  }, [cookies]);
   return (
     <div className="auth">
       <Link to="/">
